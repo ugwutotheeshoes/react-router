5be15bbdb5d983a7f1f862ed36cac837546f8dae
Emmanuel Ugwu <>
2021-08-25 16:29:47 +0100

    Made Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-    hello
+    <div className='App'>
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
